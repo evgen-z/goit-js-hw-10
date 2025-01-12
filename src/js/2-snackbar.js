795209f724f1e66fd5dbc8a18dd204f8e84916d9
evgen_zoidze <1ovva.ungrace@gmail.com>
2025-01-12 22:08:06 +0100

    init2

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,40 +1,38 @@
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
-const form = document.querySelector(".form");
+const form = document.querySelector('.form');
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-  
-   
-    const formData = new FormData(form);
-    const delay = Number(formData.get('delay'));
-    const state = formData.get('state');
-  
-   
-    createPromise(delay, state)
-      .then((result) => {
-        iziToast.success({
-          title: "✅ Success",
-          message: `Fulfilled promise in ${result}ms`,
-        });
-      })
-      .catch((error) => {
-        iziToast.error({
-          title: "❌ Error",
-          message: `Rejected promise in ${error}ms`,
-        });
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const formData = new FormData(form);
+  const delay = Number(formData.get('delay'));
+  const state = formData.get('state');
+
+  createPromise(delay, state)
+    .then(result => {
+      iziToast.success({
+        title: '✅ Success',
+        message: `Fulfilled promise in ${result}ms`,
       });
-  });
-
-  function createPromise(delay, state) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        if (state === 'fulfilled') {
-          resolve(delay);
-        } else {
-          reject(delay);
-        }
-      }, delay);
+    })
+    .catch(error => {
+      iziToast.error({
+        title: '❌ Error',
+        message: `Rejected promise in ${error}ms`,
+      });
     });
-  }
+});
+
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
